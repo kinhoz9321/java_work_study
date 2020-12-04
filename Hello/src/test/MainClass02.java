e9afd4ace39a61991440989467226e3892ccefc8
kimhongju <kinhoz9321@gmail.com>
2020-12-04 22:21:44 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/MainClass02.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MainClass02 {
+	// run 했을때 실행의 흐름이 시작되는 특별한 main 메소드 
+	public static void main(String[] args) {
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+	}
+}
+/*
+ * 알아가야 할 것
+ * 1. public 이란 무엇인가?
+ * 2. static 이란 무엇인가?
+ * 3. void 란 무엇인가?
+ */
