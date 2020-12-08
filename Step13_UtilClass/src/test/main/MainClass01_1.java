44c5269cd7a9e933690e88ec1d818929e7910f23
kimhongju <kinhoz9321@gmail.com>
2020-12-08 23:48:25 +0900

    Step13_UtilClass 주석 추가

--- Step13_UtilClass/src/test/main/MainClass01_1.java
+++ Step13_UtilClass/src/test/main/MainClass01_1.java
@@ -7,6 +7,7 @@ public class MainClass01_1 {
 		
 		Scanner scan=new Scanner(System.in);
 		System.out.println("입력 : ");
+		//nextLine()메소드는 Enter를 치기 전까지 쓴 문자열을 모두 리턴한다
 		String line=scan.nextLine();
 		System.out.println("앵무새 : "+line);
 	}
