488b5a51b7be181c3ecc52a4940ddf7507c813c4
kimhongju <kinhoz9321@gmail.com>
2020-12-11 17:17:45 +0900

    시험

---
+++ Step18_JDBC/src/test/main/MyFrame.java
@@ -0,0 +1,24 @@
+package test.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class MyFrame extends JFrame implements ActionListener{
+	
+	public MyFrame(String a) {
+		super(a);
+		JTextField f=new JTextField(10);
+		f.setText("안녕");
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
